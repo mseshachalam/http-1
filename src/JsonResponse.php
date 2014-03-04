4a72af3eac5bd5c8579e7a8804e536c8f6b59114
happyaccidents <hello@happyaccidents.me>
2014-03-04 12:54:13 +0000

    Changed license and added 4xx exception

--- src/JsonResponse.php
+++ src/JsonResponse.php
@@ -3,7 +3,7 @@
  * The Orno Component Library
  *
  * @author  Phil Bennett @philipobenito
- * @license http://www.wtfpl.net/txt/copying/ WTFPL
+ * @license MIT (see the LICENSE file)
  */
 namespace Orno\Http;
 
